52877ec4ce106f91ba51a22438572a262d964c92
Kazutoshi Yoshii <kazutoshiyoc@gmail.com>
2022-09-21 01:21:02 +0900

    秒表記の省略

--- cpu.sh
+++ cpu.sh
@@ -7,10 +7,10 @@ SAR_FORMAT_JA='[0-9][0-9]時[0-9][0-9]分[0-9][0-9]秒\s+CPU\s+%user\s+%nice\s+%
 #-------------------------------------------------------------------
 if [[ $SAR_OUTPUT =~ $SAR_FORMAT_EN ]]; then
 	echo "Hostname,Time AM/PM,%(user+system+iowait),%idle"
-	sar -P ALL | grep all | grep -e AM -e PM | awk -v sar_hostname="$SAR_HOSTNAME" 'BEGIN {OFS=","} {print sar_hostname,$1" "$2,$4+$6+$7,$9}'
+	sar -P ALL | grep all | grep -e AM -e PM | awk -v sar_hostname="$SAR_HOSTNAME" 'BEGIN {OFS=","} {print sar_hostname,substr($1,1,length($1)-3)" "$2,$4+$6+$7,$9}'
 elif [[ $SAR_OUTPUT =~ $SAR_FORMAT_JA ]]; then
 	echo "Hostname,Time AM/PM,%(user+system+iowait),%idle"
-	sar -P ALL | grep all | grep -e 秒 | awk -v sar_hostname="$SAR_HOSTNAME" 'BEGIN {OFS=","} {print sar_hostname,$1" "$2,$4+$6+$7,$9}'
+	sar -P ALL | grep all | grep -e 秒 | awk -v sar_hostname="$SAR_HOSTNAME" 'BEGIN {OFS=","} {print sar_hostname,substr($1,1,length($1)-3)" "$2,$4+$6+$7,$9}'
 else
 	echo "sar format error"
 fi
